47b5d00bee875df8ec196e7457f0a49c4bc1424d
Rory McCann <rory@technomancy.org>
2017-10-24 22:04:39 +0200

    remove debugging statements

--- src/lib.rs
+++ src/lib.rs
@@ -413,12 +413,15 @@ pub fn single_metatile(layers: &Layers, metatile: &slippy_map_tiles::Metatile, c
 
         let columns: Vec<_> = res.columns().iter().skip(1).filter(|c| c.name() != "way").collect();
 
-        let mut res = res.iter();
-        let mut res = res.take(if layer_name == "roads-low-zoom" { 1 } else { std::usize::MAX }).enumerate() ;
+        let mut res = res.iter().enumerate();
+        // 9_541 good
+        // 9_542 bad
+        //let mut res = res.take(9_542);
 
         //println!("metatile {:?}", metatile);
         for (i, row) in res {
-            let problem_obj = layer_name == "roads-low-zoom";
+            //if layer_name != "roads-low-zoom" { continue; }
+            //let problem_obj = layer_name == "roads-low-zoom" && i == 9_412 && metatile.zoom() == 5;
             //if problem_obj { println!("problem obj"); }
             //if problem_obj { println!("metatile {:?}", metatile); }
 
@@ -452,7 +455,7 @@ pub fn single_metatile(layers: &Layers, metatile: &slippy_map_tiles::Metatile, c
 
             //if problem_obj { println!("geom {:?}", geom); }
 
-            //
+            //if problem_obj { println!("geom {:?}", geom); }
 
             // TODO after converting to integer, maybe run a simple algorithm that removes points
             // which are on the line, i.e. A-B-C is straight line, so remove B. This keeps the
@@ -462,7 +465,7 @@ pub fn single_metatile(layers: &Layers, metatile: &slippy_map_tiles::Metatile, c
             let simplification: i64 = if metatile.zoom() == layers.global_maxzoom { 1 } else { 8 };
             let geom = simplify_geom(geom, simplification);
 
-            if problem_obj { println!("geom post-simplification {:?}", geom); }
+            //if problem_obj { println!("geom post-simplification {:?}", geom); }
 
             if ! is_valid(&geom) {
                 continue;
@@ -515,7 +518,7 @@ pub fn single_metatile(layers: &Layers, metatile: &slippy_map_tiles::Metatile, c
             // We want to do this in order, so we reverse the vec, and the pop from the end
             // (which is the original front).
             let mut geoms: Vec<_> = clip_geometry_to_tiles(&metatile, geom).into_iter().filter(|&(t, ref g)| g.is_some()).collect();
-            if problem_obj { println!("geoms post-clipping to tiles {:?}", geoms); }
+            //if problem_obj { println!("geoms post-clipping to tiles {:?}", geoms); }
             geoms.reverse();
 
             loop {
@@ -528,13 +531,14 @@ pub fn single_metatile(layers: &Layers, metatile: &slippy_map_tiles::Metatile, c
 
                     // Here we convert it back to i32
                     let geom: Geometry<i32> = geom.map_coords(&|&(x, y)| ( (x - (4096*i)) as i32, (y - (4096*j)) as i32 ));
-                    if problem_obj { println!("geom as i32 {:?}", geom); }
+                    //if problem_obj { println!("geom as i32 {:?}", geom); }
 
                     let feature = mapbox_vector_tile::Feature::new(geom, properties.clone());
                     let i = ((tile.x() - metatile.x())*scale + (tile.y() - metatile.y())) as usize;
                     let mvt_tile = results.get_mut(i).unwrap();
+                    //if problem_obj { println!("feature {:?}", feature); }
                     mvt_tile.add_feature(&layer_name, feature);
-                    if problem_obj { println!("mvt_tile {:?}", mvt_tile); }
+                    //if problem_obj { println!("mvt_tile {:?}", mvt_tile); }
                 }
             }
 
@@ -546,13 +550,14 @@ pub fn single_metatile(layers: &Layers, metatile: &slippy_map_tiles::Metatile, c
 
                 // Here we convert it back to i32
                 let geom: Geometry<i32> = geom.map_coords(&|&(x, y)| ( (x - (4096*i)) as i32, (y - (4096*j)) as i32 ));
-                if problem_obj { println!("geom as i32 {:?}", geom); }
+                //if problem_obj { println!("geom as i32 {:?}", geom); }
 
                 let feature = mapbox_vector_tile::Feature::new(geom, properties);
                 let i = ((tile.x() - metatile.x())*scale + (tile.y() - metatile.y())) as usize;
                 let mvt_tile = results.get_mut(i).unwrap();
+                //if problem_obj { println!("feature {:?}", feature); }
                 mvt_tile.add_feature(&layer_name, feature);
-                if problem_obj { println!("mvt_tile {:?}", mvt_tile); }
+                //if problem_obj { println!("mvt_tile {:?}", mvt_tile); }
             }
 
         }
