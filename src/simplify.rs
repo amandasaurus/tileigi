e021d5eb656da01a793fee87442415d41f49f715
Rory McCann <rory@technomancy.org>
2018-01-25 16:18:30 +0100

    attempted optimization based on using iterators

--- src/simplify.rs
+++ src/simplify.rs
@@ -123,21 +123,19 @@ fn rdp(mut points: Vec<Point<i32>>, epsilon: i32) -> Vec<Point<i32>> {
         if point1 == point2 {
             //println!("{}:{} points are the same", file!(), line!());
             // they're the same, so just look at the distance from point to all the other points
-            for (i, point) in points[start_idx+1..end_idx].iter().enumerate() {
-                if points_to_keep[i+start_idx] {
-                    let numerator = (point.x() as i64 - point1.x() as i64).pow(2) + (point.y() as i64 - point2.y() as i64).pow(2);
-                    //println!("{}:{} i {} numerator {} max_numerator {}", file!(), line!(), i, numerator, max_numerator);
+            let (max_numerator, this_index) = points[start_idx+1..end_idx].iter().enumerate()
+                .zip(points_to_keep[start_idx+1..end_idx].iter())
+                .filter_map(|((i, point), &keep)| if keep {
+                    Some(((point.x() as i64 - point1.x() as i64).pow(2) + (point.y() as i64 - point2.y() as i64).pow(2), i))
+                 } else { None } )
+                .max()
+                .unwrap();
 
-                    if numerator > max_numerator {
-                        index = i+start_idx+1;
-                        max_numerator = numerator;
-                        //println!("{}:{} using this", file!(), line!());
-                    }
-                }
-            }
+            index = this_index+start_idx+1;
+
             // In this case, the numerator is the distance from the furthest point to the original
             // point, squared. i.e. numerator = distance². So we only need to compare it with e².
-            wipe_segment = max_numerator < this_e;
+            wipe_segment = max_numerator < e;
             //println!("{}:{} max_numerator {} this_e {} wipe_segment {}", file!(), line!(), max_numerator, this_e, wipe_segment);
         } else {
             //println!("{}:{} points are different", file!(), line!());
@@ -151,22 +149,18 @@ fn rdp(mut points: Vec<Point<i32>>, epsilon: i32) -> Vec<Point<i32>> {
             debug_assert!(point_distance_sqr > 0);
             //println!("{}:{} point_distance_sqr {:?}", file!(), line!(), point_distance_sqr);
             
-
-            for (i, point) in points[start_idx+1..end_idx].iter().enumerate() {
-                if points_to_keep[i+start_idx] {
+            let (max_numerator, this_index) = points[start_idx+1..end_idx].iter().enumerate()
+                .zip(points_to_keep[start_idx+1..end_idx].iter())
+                .filter_map(|((i, point), &keep)| if keep {
                     let ac: i64 = delta_y*(point.x() as i64) + end_x_start_y;
                     let bd: i64 = delta_x*(point.y() as i64) + end_y_start_x;
-                    let numerator = (ac - bd).abs();
-                    
-                    //println!("{}:{} i {} numerator {} max_numerator {}", file!(), line!(), i, numerator, max_numerator);
+                    Some( ((ac - bd).abs(), i) )
+                 } else { None } )
+                .max()
+                .unwrap();
 
-                    if numerator > max_numerator {
-                        index = i+start_idx+1;
-                        max_numerator = numerator;
-                        //println!("{}:{} using this", file!(), line!());
-                    }
-                }
-            }
+            index = this_index+start_idx+1;
+
             assert!(e > 0);
             assert!(max_numerator > 0);
             // max_numerator is the max numerator
